00815ded57d49a490e8e974aa7ba5cf128464ce1
YC-S <yuanchenshi@gmail.com>
2017-04-09 16:17:29 -0500

    update DSD

--- main/routes/requirement.js
+++ main/routes/requirement.js
@@ -3,8 +3,17 @@ var router = express.Router();
 
 /* GET requirement page. */
 
-router.get('/', function(req, res, next) {
+router.get('/RSD', function(req, res, next) {
     res.render('requirement', {
+        title: 'High School Book Tracking Application - Requirement Specification Document'
+    });
+});
+
+
+/* GET requirement page. */
+
+router.get('/DSD', function(req, res, next) {
+    res.render('DSD', {
         title: 'High School Book Tracking Application - Requirement Specification Document'
     });
 });
