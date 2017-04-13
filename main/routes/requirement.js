b0c5c326ab7af267aa3537ed0c983de1d8079c2c
Yizhi Hong <yhong25@hawk.iit.edu>
2017-04-13 16:02:03 -0500

    RSD改名

--- main/routes/requirement.js
+++ main/routes/requirement.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET requirement page. */
 
 router.get('/RSD', function(req, res, next) {
-    res.render('requirement', {
+    res.render('RSD', {
         title: 'High School Book Tracking Application - Requirement Specification Document', users:!!req.session._id ,
         layout:'layout-login'
 
@@ -16,11 +16,7 @@ router.get('/RSD', function(req, res, next) {
 
 router.get('/DSD', function(req, res, next) {
     res.render('DSD', {
-<<<<<<< HEAD
-        title: 'High School Book Tracking Application - Design Specification Document',
-=======
         title: 'High School Book Tracking Application - Requirement Specification Document',users:!!req.session._id ,
->>>>>>> 3b9c1c8edaafb551463d0860e262b39333e11182
         layout:'layout-login'
     });
 });
